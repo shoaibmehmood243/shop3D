805e9b07f8f2bafc64a7e7ead7da2ccfa3f26f09
Shoaib Mehmood <shoaib.m@emblemtechnologies.com>
2023-06-30 14:50:36 +0500

    initial commit :tada:

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from './pages/Home';
+import Customizer from './pages/Customizer';
+import CanvasModel from './canvas/index';
+
+function App() {
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <CanvasModel />
+      <Customizer />
+    </main>
+  )
+}
+
+export default App;
